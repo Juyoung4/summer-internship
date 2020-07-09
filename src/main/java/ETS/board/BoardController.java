9183d5052656b5f73ccc3b3726b70bfadcfde637
Juyoung4 <47167335+Juyoung4@users.noreply.github.com>
2020-07-09 18:18:41 +0900

    4days

---
+++ src/main/java/ETS/board/BoardController.java
@@ -0,0 +1,257 @@
+package ETS.board;
+
+import ETS.board.service.BoardService;
+import ETS.common.service.CommonService;
+import ETS.common.util.CamelMap;
+import ETS.common.util.DataMap;
+import ETS.common.util.HttpUtil;
+import ETS.common.util.Paging;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**************************************************
+* @FileName   : BoardController.java
+* @Description: 게시판 관리 페이지 컨트롤러
+* @Author     : Seung-Jun. Kim
+* @Version    : 2020. 1. 29.
+* @Copyright  : ⓒADUP. All Right Reserved
+**************************************************/
+@Controller
+@RequestMapping("board")
+public class BoardController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@Autowired
+	private BoardService boardService;
+	
+	@Autowired
+	private CommonService commonService;
+	
+	/**************************************************
+	* @MethodName : getBoardNotice
+	* @Description: 공지사항 관리
+	* @param request
+	* @param model
+	* @param response
+	* @return String
+	* @throws Exception
+	* @Author     : Seung-Jun. Kim
+	* @Version    : 2020. 1. 29.
+	**************************************************/
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/notice", method = {RequestMethod.GET, RequestMethod.POST})
+	public String getBoardNotice(HttpServletRequest request, Model model, HttpServletResponse response) throws Exception {
+		
+		List<CamelMap> resultList = new ArrayList<CamelMap>();
+		DataMap paramMap = HttpUtil.getRequestDataMap(request);
+		
+		int rowMax = 0;
+		int pageCount = 10;
+		int rowCount = 10;
+		
+		String nowPage = paramMap.getString("nowPage");
+		
+		if (StringUtils.isEmpty(nowPage)) {
+			nowPage = "1";
+		}
+
+		paramMap.put("nowPage", nowPage);
+
+		//rowMax
+		try {
+			rowMax = boardService.getNoticeListCount(paramMap);
+		} catch (Exception e) {
+			logger.error("게시물 갯수 조회 오류 : {}", e);
+		}
+		
+		//페이지 정보
+		DataMap pageMap = Paging.initDataMapPage(rowMax, pageCount, rowCount, Integer.parseInt(nowPage));
+		
+		paramMap.put("rowCount", rowCount);
+		paramMap.put("scopeRow", pageMap.getInt("scopeRow"));
+		
+		try {
+			resultList = boardService.getNoticeList(paramMap);
+			
+		} catch (Exception e) {
+			logger.error("게시물 조회 오류 : {}", e);
+		}
+		
+		model.addAttribute("resultList", resultList);
+		model.addAttribute("pageInfo", pageMap);
+        
+		HttpUtil.getParams(paramMap, model);
+		
+		return "/board/notice";
+	}
+	
+	/**************************************************
+	* @MethodName : getBoardNoticeView
+	* @Description: 공지사항 상세보기
+	* @param request
+	* @param model
+	* @param response
+	* @return String
+	* @throws Exception
+	* @Author     : Seung-Jun. Kim
+	* @Version    : 2020. 1. 31.
+	**************************************************/
+	@RequestMapping(value = "/noticeView", method = {RequestMethod.GET, RequestMethod.POST})
+	public String getBoardNoticeView(HttpServletRequest request, Model model, HttpServletResponse response) throws Exception {
+		DataMap paramMap = HttpUtil.getRequestDataMap(request);
+		
+		CamelMap noticeInfo;
+		try {
+			noticeInfo = this.boardService.getNoticeView(paramMap);
+			
+		} catch (SQLException e) {
+			noticeInfo = new CamelMap();
+			logger.debug("notice read", e);
+		}
+		model.addAttribute("noticeInfo", noticeInfo);
+		
+		return "/board/noticeView";
+	}
+	
+	/**************************************************
+	* @MethodName : getBoardNoticeWrite
+	* @Description: 공지사항 등록화면
+	* @param request
+	* @param model
+	* @param response
+	* @return String
+	* @throws Exception
+	* @Author     : Seung-Jun. Kim
+	* @Version    : 2020. 2. 4.
+	**************************************************/
+	@RequestMapping(value = "/noticeWrite", method = {RequestMethod.GET, RequestMethod.POST})
+	public String getBoardNoticeWrite(HttpServletRequest request, Model model, HttpServletResponse response) throws Exception {
+		
+		CamelMap noticeInfo = new CamelMap();
+		DataMap paramMap = HttpUtil.getRequestDataMap(request);
+		
+		String noticeSeq = paramMap.getString("noticeSeq");
+		String crud = "INS";
+		
+		if (StringUtils.isNotEmpty(noticeSeq)) crud = "MOD";
+		
+		try {
+			if ("MOD".equals(crud)) {
+				noticeInfo = boardService.getNoticeView(paramMap);
+			}
+		} catch (Exception e) {
+			logger.error("게시물 조회 오류 : {}", e);
+		}
+		
+		model.addAttribute("noticeInfo", noticeInfo);
+		model.addAttribute("crud", crud);
+		
+		HttpUtil.getParams(paramMap, model);
+		
+		return "/board/noticeWrite";
+	}
+	
+
+	/*
+	 * 
+	 * 
+	 * 
+	 *  등록 , 수정, 삭제
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	/**************************************************
+	* @MethodName : ajaxNoticeDelete
+	* @Description: 공지사항 삭제
+	* @param request
+	* @param response
+	* @param model
+	* @return Model
+	* @throws IOException
+	* @Author     : Seung-Jun. Kim
+	* @Version    : 2020. 1. 31.
+	**************************************************/
+	@RequestMapping(value = {"/ajaxNoticeDelete"}, method = RequestMethod.POST)
+	@ResponseBody
+    public Model ajaxNoticeDelete(HttpServletRequest request, HttpServletResponse response, Model model) throws IOException {
+		DataMap paramMap = HttpUtil.getRequestDataMap(request);
+		
+		boolean sw = false;
+		
+		try {
+			sw = this.boardService.deleteNotice(paramMap);
+		} catch (Exception e) {
+			logger.error("공지사항 삭제 오류 : {}", e);
+		}
+		
+		model.addAttribute("sw", sw);
+		
+		return model;
+	}
+	
+	/**************************************************
+	* @MethodName : ajaxNoticeSubmit
+	* @Description: 공지사항 등록
+	* @param request
+	* @param model
+	* @return Model
+	* @Author     : Seung-Jun. Kim
+	* @Version    : 2020. 2. 4.
+	**************************************************/
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = { "/ajaxNoticeSubmit" }, method = {RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public Model ajaxNoticeSubmit(HttpServletRequest request, Model model) {
+		DataMap paramMap = HttpUtil.getRequestDataMap(request);
+
+		boolean sw = false;
+		int result = 0;
+
+		paramMap.put("filePath", "BOARD");
+		paramMap.put("imgNm", "");
+		
+		List<DataMap> rstFileList = null;
+		
+		try {
+			rstFileList = commonService.saveFile(request, paramMap);
+			for (DataMap fileMap : rstFileList) {
+				
+				logger.info("fileMap : {}", fileMap);
+				paramMap.put("imgNm", fileMap.get("saveFilePath") + "/" + fileMap.get("saveFileName"));
+				paramMap.put("orgImgNm", fileMap.get("realFileName"));
+			}
+			
+			if (!"MOD".equals(paramMap.getString("crud"))){
+				result = this.boardService.insertNotice(paramMap);
+			} else {
+				result = this.boardService.updateNotice(paramMap);
+			}
+			
+			sw = (result==1);
+		} catch (Exception e1) {
+			logger.debug("공지사항 저장 오류", e1);
+		}
+		model.addAttribute("sw", sw);
+		return model;
+	}
+}
---
+++ src/main/webapp/views/4/notice.ftl
@@ -0,0 +1,149 @@
+<#assign pagingTag = JspTaglibs["/WEB-INF/tld/paging.tld"] />
+<#setting number_format = "###"/>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#checkAll").click(function(){
+			if($("#checkAll").prop("checked")){
+				$("input[name=chk]").prop("checked",true);
+			}else{
+				$("input[name=chk]").prop("checked",false);
+			}
+		});
+	});
+	
+	function fn_delete(){
+		if($('input:checkbox[name=chk]').is(':checked') == false){
+			jsAlert.alert("알림", "삭제할 게시물을 선택해주세요");
+			return false;
+		}
+		jsAlert.confirm("확인", "해당 게시물을 삭제 하시겠습니까?", fn_deleteYn, null, "확인", "취소");
+    }
+	
+	function fn_deleteYn(){
+		var seqs = [];
+		$("input[name=chk]:checked").each(function() {
+			seqs[seqs.length] = $(this).val();
+		});
+		
+		$("#noticeSeq").val(seqs.join(","));
+		
+		ajaxFormExecute("noticeFrm", "/board/ajaxNoticeDelete", "post", false, false, ajaxReturnDelete);
+	}
+	
+	function ajaxReturnDelete(data){
+		if(data.sw){
+    		jsAlert.alert("알림", "삭제되었습니다.", null, fn_list, "확인");
+    	}else{
+    		jsAlert.alert("알림", "삭제에 실패하였습니다.<br>관리자에게 문의바랍니다.");
+    	}
+	}
+	
+	function fn_list(){
+		$("#noticeFrm").prop("action", "/board/notice");
+		$('#nowPage').val('1');
+ 		formSubmit("noticeFrm");
+	}
+	
+	function fn_view(noticeSeq){
+		$("#noticeFrm").prop("action", "/board/noticeView");
+		$('#noticeSeq').val(noticeSeq);
+ 		formSubmit("noticeFrm");
+	}
+	
+	function fn_write(){
+		location.href="/board/noticeWrite";	
+	}
+	
+	function fn_search(){
+	
+	}
+	
+</script>
+ 
+<form class="form-inline" name="noticeFrm" id="noticeFrm" method="post" >
+	<input type="hidden" id="noticeSeq" name="noticeSeq" value="" />
+</form>
+
+<div class="right_col" role="main">
+	<div class="">
+    	<div class="page-title">
+  			<div class="title_left">
+				<h3>공지사항</h3>
+			</div>
+		</div>
+	
+		<div class="clearfix"></div>
+	
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="x_panel">
+					<div class="x_content">
+						<div class="row">
+							<div class="col-md-6 col-sm-6 col-xs-6 text-left">
+								<button type="button" class="btn btn-danger" onclick="fn_delete();">삭제</button>
+							</div>
+							<div class="col-md-6 col-sm-6 col-xs-6 text-right">
+								<button type="button" class="btn btn-primary" onclick="fn_write();">등록</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="clearfix"></div>
+
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="x_panel">
+					<div class="x_content">
+						<div class="table-responsive">
+							<table class="table table-striped jambo_table bulk_action">
+								<thead>
+									<tr class="headings">
+										<th class="column-title"><input type="checkbox" id="checkAll" name="checkAll" /></th>
+										<th class="column-title">번호</th>
+										<th class="column-title">제목</th>
+										<th class="column-title">작성일자 </th>
+									</tr>
+								</thead>
+								<tbody>
+									<#if resultList?has_content>
+									<#assign rNum = pageInfo.rowMax - pageInfo.startRowNum + 1 />
+									<#list resultList as rMap>
+									<tr style="cursor:pointer;">
+										<td class=" "><input type="checkbox" name="chk" value="${rMap.noticeSeq!}" /></td>
+										<td class="even pointer" onclick="javascript:fn_view('${rMap.noticeSeq}')">${rNum}</td>
+										<td class="even pointer" onclick="javascript:fn_view('${rMap.noticeSeq}')">${rMap.noticeTitle!}</td>
+										<td class="even pointer" onclick="javascript:fn_view('${rMap.noticeSeq}')">${rMap.regDate!}</td>
+									</tr>
+									<#assign rNum = rNum - 1/>
+									</#list>
+									<#else>
+										<tr>
+											<td colspan="4"><strong>게시물이 존재하지 않습니다.</strong></td>
+										</tr>
+									</#if>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div class="form-inline">
+					<select id="searchType" name="searchType">
+						<option value="">검색조건</option>
+						<option value="t">제목</option> 
+						<option value="c">내용</option>
+						<option value="n">번호</option>
+					</select>
+					<input class="form-control" type="text" id="keyword" name="keyword" placeholder="검색어를 입력하세요"/>
+					<button id="searchBtn" class="btn btn-primary" onclick="javascript:fn_search('${rMap.noticeSeq}')">${rNum}<>Search</button>
+				</div>
+				<nav aria-label="Page navigation example" class="content-center text-center">
+					<@pagingTag.Paging url="/board/notice" pageCount="${pageInfo.pageCount!}" rowCount="${pageInfo.rowCount!}" rowMax="${pageInfo.rowMax!}" nowPage="${pageInfo.nowPage!}" params="" />
+				</nav>
+			</div>
+		</div>
+	</div>
+</div>
